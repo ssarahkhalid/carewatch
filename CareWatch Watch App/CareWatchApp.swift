fe64e5ba0e828b02b6481c8fc798bbf9e72c469d
ssarahkhalid <88570249+ssarahkhalid@users.noreply.github.com>
2024-12-07 10:47:24 -0500

    Initial Commit

---
+++ CareWatch Watch App/CareWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareWatchApp.swift
+//  CareWatch Watch App
+//
+//  Created by sarah khalid on 2024-12-07.
+//
+
+import SwiftUI
+
+@main
+struct CareWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CareWatch Watch App
+//
+//  Created by sarah khalid on 2024-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
